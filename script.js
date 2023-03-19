66d216fe0b3ff08726260965618408b3c23e0114
Vaishnavi1328 <113687715+Vaishnavi1328@users.noreply.github.com>
2023-03-19 08:12:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const myBirth = "13 Aug 2023";
+
+function countdown(){
+    const myNewBirth = new Date(myBirth);
+    const curnDate = new Date();
+    
+    const totalSeconds = (myNewBirth - curnDate)/1000;
+
+    const days = Math.floor(totalSeconds/3600/24);
+    const hours = Math.floor(totalSeconds/3600) % 24;
+    const minutes = Math.floor(totalSeconds/60)%60;
+    const seconds = Math.floor(totalSeconds%60);
+
+
+    console.log(days,hours,minutes,seconds);
+    document.getElementById('days1').innerHTML= days;
+    document.getElementById('hours1').innerHTML= hours;
+    document.getElementById('min1').innerHTML= minutes;
+    document.getElementById('sec1').innerHTML= seconds;
+}
+countdown();
+setInterval(countdown,1000);
